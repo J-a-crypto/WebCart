ea94ac826623674808dcfa08b40dd7dd47626bf7
Anthony Zaccardi <72520954+darkHorse337@users.noreply.github.com>
2023-12-05 13:32:34 -0500

    Add files via upload
    new main.py and webcart.py

--- WebCartProject/main.py
+++ WebCartProject/main.py
@@ -3,5 +3,4 @@ from webcart import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
--- WebCartProject/webcart.py
+++ WebCartProject/webcart.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 # tech products data
 techProducts = [
@@ -7,7 +8,7 @@ techProducts = [
         'description': 'Powerful laptop for all your computing needs.',
         'price': 999.99,
         'imageURL': 'https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?cs=srgb&dl=pexels-craig-dennis-205421.jpg&fm=jpg'
-        
+
     },
     {
         'name': 'Laptop',
@@ -15,13 +16,13 @@ techProducts = [
         'price': 999.99,
         'imageURL': 'https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?cs=srgb&dl=pexels-craig-dennis-205421.jpg&fm=jpg'
     },
-     {
+    {
         'name': 'Laptop',
         'description': 'Powerful laptop for all your computing needs.',
         'price': 999.99,
         'imageURL': 'https://images.pexels.com/photos/205421/pexels-photo-205421.jpeg?cs=srgb&dl=pexels-craig-dennis-205421.jpg&fm=jpg'
     },
-     {
+    {
         'name': 'Laptop',
         'description': 'Powerful laptop for all your computing needs.',
         'price': 999.99,
@@ -29,22 +30,30 @@ techProducts = [
     },
     # ... other product data
 ]
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLAlchemy_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+    db = SQLAlchemy(app)
+    db.init_app(app)
 
-@app.route('/')
-def index():
-    return render_template('product.html', products=techProducts)
 
-# API endpoint to handle adding products to cart
-@app.route('/add_to_cart/<int:index>')
-def add_to_cart(index):
-    
-    selected_product = techProducts[index]
-   
-    cart = []
-    cart.append(selected_product)
-    return jsonify({'message': 'Added to cart successfully'})
+    @app.route('/products', methods=["GET", "POST"])
+    def index():
+        return render_template('product.html', products=techProducts)
 
-def calculate_cart_total(techProducts):
-    total_price = sum(item["price"] for item in techProducts)
-    return f'Total Price: ${total_price: .2f}'
 
+    # API endpoint to handle adding products to cart
+    @app.route('/add_to_cart/<int:index>')
+    def add_to_cart(index):
+        selected_product = techProducts[index]
+
+        cart = []
+        cart.append(selected_product)
+        return jsonify({'message': 'Added to cart successfully'})
+
+
+    def calculate_cart_total(techProducts):
+        total_price = sum(item["price"] for item in techProducts)
+        return f'Total Price: ${total_price: .2f}'
+
+    return app
