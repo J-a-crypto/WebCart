88e68588fd5be0669009a5c11896e4cc59b840c6
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2023-12-06 23:16:31 -0500

    Update server.py
    I couldn't get the SQLAlchemy to work so I decided to use my code here.

--- WebCartProject/server.py
+++ WebCartProject/server.py
@@ -1,38 +1,44 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template,redirect,url_for
 from sql_connection import get_sql_connection
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 #SQL Authentication
 connection = get_sql_connection()
 
-@app.route('/')
-
-@app.route('/getData', methods=['GET'])
-def get_products():
-    products = get_all_products(connection)
-    response = jsonify(products)
-    return response
-
+#This gets all the product info and puts them in a response array.
 def get_all_products(connection):
     cursor = connection.cursor()
     
-    query = ("select products.id, products.name, products.company, ")
+    query = ("SELECT * FROM shop.products")
     
     cursor.execute(query)
     
-    response =[]
+    response = []
     
-    for(id, name, company, description, price, imageURL) in cursor:
+    for(id, name, description, price, imageURL,company) in cursor:
         response.append(
             {
             'id': id,
             'name': name,
-            'company' : company,
             'description' : description,
             'price' : price,
-            'imageURL' : imageURL
+            'imageURL' : imageURL,
+            'company' : company,
             }
         )
     return response
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('get_products'))
+
+#This will get the array of products and ref them as products which will be used in the browse.html template.
+@app.route('/get_products', methods=['GET'])
+def get_products():
+   products = get_all_products(connection)
+   return render_template('browse.html',products=products)
+if __name__ == '__main__':
+    app.run(debug=True)
     
