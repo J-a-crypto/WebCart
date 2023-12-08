7624e801ef66a862e280c83bd000cbfa814e0b9c
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2023-12-08 04:25:15 -0500

    Update server.py

--- WebCartProject/server.py
+++ WebCartProject/server.py
@@ -1,13 +1,10 @@
 from flask import Flask, request, jsonify,render_template,redirect,url_for,session
 from sql_connection import get_sql_connection
-import os
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'Japupalo2003'
 #SQL Authentication
 connection = get_sql_connection()
-
-cart=[]
 
 def get_all_products(connection):
     cursor = connection.cursor()
@@ -30,9 +27,31 @@ def get_all_products(connection):
             }
         )
     return response
+
+def get_product_by_id(connection, product_id):
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM shop.products WHERE ProductID = %s"
+    cursor.execute(query, (product_id,))
+
+    product = cursor.fetchone()
+
+    if product:
+        id, name, description, price, imageURL, company = product
+        product_details = {
+            'id': id,
+            'name': name,
+            'description': description,
+            'price': price,
+            'imageURL': imageURL,
+            'company': company,
+        }
+        return product_details
+
+    return None
 @app.route('/')
 def index():
-    return redirect(url_for('get_products'))
+    return redirect(url_for('login'))
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error_message = None
@@ -43,6 +62,7 @@ def login():
         
         if username == 'Japupalo' and password == '123':
             session['logged_in'] = True
+            session['cart'] = []
             return redirect(url_for('get_products'))
         else:
             error_message = "Incorrect Username or Password. Please try again!"
@@ -58,20 +78,36 @@ def get_products():
    return render_template('browse.html', products=products)
 
   # API endpoint to handle adding products to cart
-@app.route('/add_to_cart/<int:index>')
-def add_to_cart(index):
-    selected_product = index
-    cart.append(selected_product)
-    return jsonify({'message': 'Added to cart successfully'})
+@app.route('/add_to_cart/<product_id>', methods=['GET'])
+def add_to_cart(product_id):
+    product = get_product_by_id(connection, product_id)
+    if product:
+     # Initialize cart if not present in session
+        session['cart'].append(product)
+        
+        print("cart contents:", session['cart'])
+        
+        return jsonify({'success': True, 'message': 'Product added to cart successfully'})
+    else:
+        return jsonify({'success': False, 'message': 'Product not found'}), 404
+    
 
 
-#still trying to figure this out with the new examples we got
-#I don't think this works because the products is in the function get_products, not global
-def calculate_cart_total(Products):
-    total_price = sum(product["price"] for product in Products)
-    return f'Total Price: ${total_price: .2f}'
+@app.route('/checkout', methods=['GET', 'POST'])
+def checkout():
 
+    cart = session.get('cart', [])  # Retrieve the user's cart from the session
 
+    print('Cart contents:', cart)
+    app.logger.debug("cart conntents: %s", cart)
+    
+    if request.method == 'POST':
+        # Perform checkout logic, e.g., update database, send confirmation email, etc.
+        # Clear the cart after checkout
+        session['cart'] = []
+        return redirect(url_for('get_products'))
+
+    return render_template('checkout.html', cart=cart)
 
 if __name__ == '__main__':
     app.run(debug=True)
