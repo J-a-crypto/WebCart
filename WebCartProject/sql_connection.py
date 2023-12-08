7259332d441cc308cc45b42ce6695a79a1f62041
Justin Apupalo <69662609+J-a-crypto@users.noreply.github.com>
2023-12-07 23:34:57 -0500

    Update sql_connection.py

--- WebCartProject/sql_connection.py
+++ WebCartProject/sql_connection.py
@@ -1,12 +1,16 @@
 import mysql.connector
 
-_cnx = None
+__cnx = None
 
 #This will be the authentication of the SQL connection between our server and db
 def get_sql_connection():
     global __cnx 
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root',password='...',
-                                        host = '127.0.0.1',
-                                        database = '...')
+        __cnx = mysql.connector.connect(
+        user='root',
+        password='Japupalo2003$',
+        host='127.0.0.1',
+        database='shop',
+        auth_plugin='mysql_native_password'
+)
     return __cnx
